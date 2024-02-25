f89110ea443bd99c1f74813374fa6898c70566bd
wanglihuaya <wanglihuaya@outlook.com>
2024-02-25 10:39:04 +0800

    feat: plan b

--- server/server.js
+++ server/server.js
@@ -284,7 +284,7 @@ function loadData() {
   let cfgData = {};
 
   // curData.users = loadXML(path.join(cwd, "data/users.xlsx"));
-  curData.users = loadXML(path.join(dataBath, "../../output/users.xlsx"));
+  curData.users = loadXML(path.join(dataBath, "/data/users.xlsx"));
   // 重新洗牌
   shuffle(curData.users);
 
